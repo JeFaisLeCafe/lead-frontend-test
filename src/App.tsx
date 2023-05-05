da1612f17157908ec9f111c8c9d5b682bbf4595b
Pierre-Etienne Soury <pierretienne.soury@gmail.com>
2023-05-05 14:45:04 +0200

    feat: display raw order data for order #UK1876YH08_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { BASE_API_URL } from "./links";
+import axios from "axios";
+import { ShipupOrder } from "./type";
+
+const key = import.meta.env.VITE_API_KEY;
+
+axios.defaults.baseURL = BASE_API_URL;
+axios.defaults.headers.common["Authorization"] = key;
 
 function App() {
+  const [data, setData] = useState<ShipupOrder>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiResponse = await axios.get<ShipupOrder>("orders", {
+        params: { order_number: "UK1876YH08_2" }
+      });
+      setData(apiResponse.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
-      <p>Hello World ! </p>
+      <p>Data for order #UK1876YH08_2</p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
